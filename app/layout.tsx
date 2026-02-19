d79cddb554e0a85e36abc8536e85e7dbf2fe1044
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2026-02-20 00:35:57 +0100

    feat: add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,106 @@ export default function RootLayout({
         <HeaderWithAuth />
         {children}
 
+        <footer className="fr-footer" role="contentinfo" id="footer">
+          <div className="fr-container">
+            <div className="fr-footer__body">
+              <div className="fr-footer__brand fr-enlarge-link">
+                <a
+                  href="/"
+                  title="Retour à l'accueil du site - Documents d'Architecture - République Française"
+                >
+                  <p className="fr-logo">
+                    République
+                    <br />
+                    Française
+                  </p>
+                </a>
+              </div>
+              <div className="fr-footer__content">
+                <ul className="fr-footer__content-list">
+                  <li className="fr-footer__content-item">
+                    <a
+                      title="info.gouv.fr - nouvelle fenêtre"
+                      href="https://info.gouv.fr"
+                      target="_blank"
+                      rel="noopener external"
+                      className="fr-footer__content-link"
+                    >
+                      info.gouv.fr
+                    </a>
+                  </li>
+                  <li className="fr-footer__content-item">
+                    <a
+                      title="service-public.gouv.fr - nouvelle fenêtre"
+                      href="https://service-public.gouv.fr"
+                      target="_blank"
+                      rel="noopener external"
+                      className="fr-footer__content-link"
+                    >
+                      service-public.gouv.fr
+                    </a>
+                  </li>
+                  <li className="fr-footer__content-item">
+                    <a
+                      title="legifrance.gouv.fr - nouvelle fenêtre"
+                      href="https://legifrance.gouv.fr"
+                      target="_blank"
+                      rel="noopener external"
+                      className="fr-footer__content-link"
+                    >
+                      legifrance.gouv.fr
+                    </a>
+                  </li>
+                  <li className="fr-footer__content-item">
+                    <a
+                      title="data.gouv.fr - nouvelle fenêtre"
+                      href="https://data.gouv.fr"
+                      target="_blank"
+                      rel="noopener external"
+                      className="fr-footer__content-link"
+                    >
+                      data.gouv.fr
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="fr-footer__bottom">
+              <ul className="fr-footer__bottom-list">
+                <li className="fr-footer__bottom-item">
+                  <a href="#" className="fr-footer__bottom-link">
+                    Accessibilité : non conforme
+                  </a>
+                </li>
+                <li className="fr-footer__bottom-item">
+                  <a href="#" className="fr-footer__bottom-link">
+                    Mentions légales
+                  </a>
+                </li>
+                <li className="fr-footer__bottom-item">
+                  <a href="#" className="fr-footer__bottom-link">
+                    Données personnelles
+                  </a>
+                </li>
+              </ul>
+              <div className="fr-footer__bottom-copy">
+                <p>
+                  Sauf mention explicite de propriété intellectuelle détenue par
+                  des tiers, les contenus de ce site sont proposés sous{" "}
+                  <a
+                    href="https://github.com/etalab/licence-ouverte/blob/master/LO.md"
+                    target="_blank"
+                    rel="noopener external"
+                    title="Licence etalab-2.0 - nouvelle fenêtre"
+                  >
+                    licence etalab-2.0
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </footer>
+
         <Script src="/dsfr/dsfr.module.min.js" strategy="afterInteractive" />
       </body>
     </html>
